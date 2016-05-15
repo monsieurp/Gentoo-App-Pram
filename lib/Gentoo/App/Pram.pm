6539ab9cc97a25f53ec4fc26802901dea44c0110
Patrice Clement <monsieurp@gentoo.org>
2016-05-15 19:13:25 +0000

    License is actually P5 because we love it.

--- lib/Gentoo/App/Pram.pm
+++ lib/Gentoo/App/Pram.pm
@@ -177,22 +177,11 @@ Patrice Clement <monsieurp@gentoo.org>
 
 version 0.001
 
-=head1 LICENSE
-
-Copyright (C) 2015 Patrice Clement
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+=head1 COPYRIGHT AND LICENSE
+ 
+This software is copyright (c) 2016 by Patrice Clement.
+ 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
